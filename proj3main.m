1625e7c85a3343b66678479c441d01a1e651acbb
Ethan Davis <exd5220@psu.edu>
2024-04-19 01:57:25 -0400

    working now

--- proj3main.m
+++ proj3main.m
@@ -2,7 +2,7 @@ function [outjpeg] = proj3main(dirstring, maxframenum, abs_diff_threshold, alpha
 %runs multiple motion detection algorithms on a given set of jpegs stored
 %in dirstring according to the other parameters
 
-%read files into array jpeg
+%read files into cell array, jpegs
 file = dir(dirstring);
 for i=1:maxframenum
     file_name = [dirstring file(i+2).name];
@@ -12,16 +12,19 @@ end
 
 
 %run motion detection algos
-BGS = BGSum(jpegs,abs_diff_threshold,maxframenum);
-FD = FrameDiff(jpegs,abs_diff_threshold,maxframenum);
-ABGS = ABGSum(jpegs,abs_diff_threshold,maxframenum,alpha_parameter);
-PFD = PFrameDiff(jpegs,abs_diff_threshold,maxframenum,gamma_parameter);
 
+lamda = abs_diff_threshold/255;
+BGS = BGSum(jpegs,lamda,maxframenum);
+FD = FrameDiff(jpegs,lamda,maxframenum);
+ABGS = ABGSum(jpegs,lamda,maxframenum,alpha_parameter);
+PFD = PFrameDiff(jpegs,lamda,maxframenum,gamma_parameter);
+
+%return output in case use wants to do further work on ans
 for i=1:maxframenum
-    outjpeg{i} = [BGS{i} FD{i};ABGS{i} PFD{i}];
+    outjpeg{i} = [BGS{i}(:,:,1) FD{i}(:,:,1);ABGS{i}(:,:,1) PFD{i}(:,:,1)];
 
 end
-
+%save outputs into file
 
 
 
